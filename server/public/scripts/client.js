485d99e907a4ade0932a9ee353cbcb224f67273e
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-12-01 17:00:34 -0600

    added images

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+var app = angular.module('GalleryApp', []);
+
+app.controller('GalleryController', ['$http', function($http){ // brought in $http as a dependency
+    console.log('Gallery Controller has been loaded');
+    var self = this;
+
+
+}]);
