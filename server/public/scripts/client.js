658edb6ed0cf36c717a6d0163ae2a1e27dff9e13
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-12-03 21:16:42 -0600

    image description shows up under photo

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,56 @@ app.controller('GalleryController', ['$http', function($http){ // brought in $ht
     var self = this;
 
 
+
+self.photoArray = [
+    {
+        url:'images/yirgy_bw.jpg',
+        description: 'My old dog yirgy. He was my first dog and very important to me. Yirgy taught me a lot about life.',
+        visable: true
+        
+    },
+    {
+        url:'images/sarah_adriatic.jpg',
+        description: 'My partner, Sarah. My favorite person.',
+        visable: true,
+    },
+    {
+        url:'images/kevin_winter_porch.jpg',
+        description: 'Kevin is a doofus dog but we love him very much. Also, quite handsome.',
+        visable: true
+    },
+    {
+        url: 'images/lake-superior.jpg',
+        description: 'Lake superior, reminds me of the vastness of the world.',
+        visable: true
+    },
+    {
+        url:'images/gooseberry.jpg',
+        description: 'Gooseberry Falls. I go up north at least once a year to camp for three or four days with the dog. It is a very healing experience for me.',
+        visable: true
+    },
+    {
+        url: 'images/korcula.jpg',
+        description:'This is from Korcula, and island off the Croatian coast. Travel is important to me and I hope to be able to to more of it again in the future.',
+        visable: true
+    }
+]
+
+
+
+
+
+self.countClicks = function(){
+    var clicks = 0;
+    clicks++;
+    console.log('clicks');
+    
+}
+
+
+
+self.getImageDescription = function(image){
+    image.visible = image.visable ? false : true;
+    
+}
 }]);
