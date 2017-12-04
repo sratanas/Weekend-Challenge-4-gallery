30ab3175ee0e8d7dc4d51579f2219e5542dfead1
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-12-03 21:36:51 -0600

    MESSAGE

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,34 +9,28 @@ app.controller('GalleryController', ['$http', function($http){ // brought in $ht
 self.photoArray = [
     {
         url:'images/yirgy_bw.jpg',
-        description: 'My old dog yirgy. He was my first dog and very important to me. Yirgy taught me a lot about life.',
-        visable: true
+        description: 'My old dog yirgy. He was my first dog and very important to me. Yirgy taught me a lot about life.'
         
     },
     {
         url:'images/sarah_adriatic.jpg',
-        description: 'My partner, Sarah. My favorite person.',
-        visable: true,
+        description: 'My partner, Sarah. My favorite person.'
     },
     {
         url:'images/kevin_winter_porch.jpg',
-        description: 'Kevin is a doofus dog but we love him very much. Also, quite handsome.',
-        visable: true
+        description: 'Kevin is a doofus dog but we love him very much. Also, quite handsome.'
     },
     {
         url: 'images/lake-superior.jpg',
-        description: 'Lake superior, reminds me of the vastness of the world.',
-        visable: true
+        description: 'Lake superior, reminds me of the vastness of the world.'
     },
     {
         url:'images/gooseberry.jpg',
-        description: 'Gooseberry Falls. I go up north at least once a year to camp for three or four days with the dog. It is a very healing experience for me.',
-        visable: true
+        description: 'Gooseberry Falls. I go up north at least once a year to camp for three or four days with the dog. It is a very healing experience for me.'
     },
     {
         url: 'images/korcula.jpg',
-        description:'This is from Korcula, and island off the Croatian coast. Travel is important to me and I hope to be able to to more of it again in the future.',
-        visable: true
+        description:'This is from Korcula, and island off the Croatian coast. Travel is important to me and I hope to be able to to more of it again in the future.'
     }
 ]
 
